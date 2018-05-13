6016358496d775f3620ec1e9567515ef3f85172e
Chris D'Aloisio <chris@creativecontrol.com.au>
2018-05-13 14:54:55 +1000

    Created an implement method for creating a concrete implementation of an interface

--- lib/interfacer.rb
+++ lib/interfacer.rb
@@ -1,6 +1,9 @@
 require "interfacer/version"
 require "helpers/symbol_helpers"
 
+class MissingInterface < StandardError; end
+class MissingImplementations < StandardError; end
+class AdditionalMethodsDetected < StandardError; end
 
 class InterfaceDefinitionProxy
   attr_reader :defined_public_methods, :defined_private_methods
@@ -23,8 +26,21 @@ class InterfaceDefinitionProxy
   end
 end
 
+class ImplementationDefinitionProxy
+  attr_reader :defined_public_methods, :defined_private_methods
+
+  def initialize
+    @defined_public_methods = []
+    @defined_private_methods = []
+  end
+
+  def method_missing(name, *args, &block)
+    @defined_public_methods << [name, block]
+  end
+end
+
 module Interfacer
-  def self.build(class_sym, &block)
+  def self.build(module_sym, &block)
     modjule = Module.new
 
     definition_proxy = InterfaceDefinitionProxy.new
@@ -38,8 +54,39 @@ module Interfacer
       modjule.send(:private, name, definition)
     end
 
-    classified_name = class_sym.classify
-    Object.const_set classified_name, modjule
+    modularized_name = module_sym.classify
+    Object.const_set modularized_name, modjule
+    Object.const_get modularized_name
+  end
+
+  def self.implement(module_sym, for_class:, &block)
+    modularized_name = module_sym.classify
+    modjule = Object.const_get modularized_name
+
+    klass = Class.new
+
+    definition_proxy = ImplementationDefinitionProxy.new
+    raise ArgumentError, 'Must provide a block' unless block_given?
+    definition_proxy.instance_eval(&block)
+
+    public_definitions_diff = modjule.instance_methods.sort - definition_proxy.defined_public_methods.map(&:first).sort
+    if public_definitions_diff.count.positive?
+      raise MissingImplementations, "Missing definitions for #{public_definitions_diff}"
+    end
+
+    public_definitions_diff = definition_proxy.defined_public_methods.map(&:first).sort - modjule.instance_methods.sort
+    if public_definitions_diff.count.positive?
+      raise AdditionalMethodsDetected, "Additional definitions detected, please remove #{public_definitions_diff}"
+    end
+
+    definition_proxy.defined_public_methods.each do |name, definition|
+      klass.send(:define_method, name, definition)
+    end
+
+    classified_name = for_class.classify
+    Object.const_set classified_name, klass
     Object.const_get classified_name
+  rescue NameError
+    raise MissingInterface, modularized_name
   end
 end
--- spec/interfacer_spec.rb
+++ spec/interfacer_spec.rb
@@ -18,4 +18,96 @@ RSpec.describe Interfacer do
       expect { Class.new.extend(build).send(:example_private) }.to raise_error(NotImplementedError)
     end
   end
+
+  describe '.implement' do
+    subject(:implement) do
+      implementation
+    end
+
+    before do
+      Interfacer.build(:some_interface) do
+        def_public_methods(:example_string, :example_integer)
+      end
+    end
+
+    context 'when interface does not exist' do
+      let(:implementation) do
+        Interfacer.implement(:non_existent_interface, for_class: :something_else)
+      end
+
+      specify { expect { implement }.to raise_error(MissingInterface, 'NonExistentInterface') }
+    end
+
+    context 'when interface does exist' do
+      let(:implementation) do
+        Interfacer.implement(:some_interface, for_class: :something_else) do
+          example_string do
+            'foo'
+          end
+
+          example_integer do
+            1
+          end
+        end
+      end
+
+      specify { expect { implement }.not_to raise_error }
+    end
+
+    context 'when an implementation has been written' do
+      let(:implementation) do
+        Interfacer.implement(:some_interface, for_class: :something_else) do
+          example_string do
+            'foo'
+          end
+
+          example_integer do
+            1
+          end
+        end
+      end
+
+      it 'works', :aggregate_failures do
+        expect(implement.new.example_string).to eq 'foo'
+        expect(implement.new.example_integer).to eq 1
+      end
+    end
+
+    context 'when additional methods have been added to implementation' do
+      let(:implementation) do
+        Interfacer.implement(:some_interface, for_class: :something_else) do
+          example_string do
+            'foo'
+          end
+
+          example_integer do
+            1
+          end
+
+          too_many_methods do
+          end
+        end
+      end
+
+      specify do
+        expect { implement }
+          .to raise_error(AdditionalMethodsDetected, include('[:too_many_methods]'))
+      end
+    end
+
+    context 'when not all implementations have been written' do
+      let(:implementation) do
+        Interfacer.implement(:some_interface, for_class: :something_else) do
+          example_string do
+            "some string"
+          end
+        end
+      end
+
+      specify do
+        expect { implement }
+          .to raise_error(MissingImplementations, include('[:example_integer]'))
+      end
+    end
+  end
 end
