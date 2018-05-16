40973fa1f4a9967d3030399eb7ec100f7bb82730
Chris D'Aloisio <chris@creativecontrol.com.au>
2018-05-16 23:58:18 +1000

    Refactor Interface.build method

--- lib/interfacer/interface.rb
+++ lib/interfacer/interface.rb
@@ -6,23 +6,20 @@ module Interfacer
       definition_proxy = InterfaceDefinitionProxy.new
       definition_proxy.instance_eval(&block)
 
-      definition_proxy.defined_public_methods.each do |name, definition|
-        modjule.send(:define_method, name, definition)
-      end
-
-      definition_proxy.defined_protected_methods.each do |name, definition|
-        modjule.send(:define_method, name, definition)
-        modjule.send(:protected, name)
-      end
-
-      definition_proxy.defined_private_methods.each do |name, definition|
-        modjule.send(:define_method, name, definition)
-        modjule.send(:private, name)
-      end
+      define_methods_with_visibility(:public, definition_proxy.defined_public_methods, modjule)
+      define_methods_with_visibility(:protected, definition_proxy.defined_protected_methods, modjule)
+      define_methods_with_visibility(:private, definition_proxy.defined_private_methods, modjule)
 
       modularized_name = module_sym.classify
       Object.const_set modularized_name, modjule
       Object.const_get modularized_name
+    end
+
+    private_class_method def self.define_methods_with_visibility(visibility, methods, modjule)
+      methods.each do |name, definition|
+        modjule.send(:define_method, name, definition)
+        modjule.send(visibility, name)
+      end
     end
   end
 end
