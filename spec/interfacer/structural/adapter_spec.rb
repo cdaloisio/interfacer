d34ac5e0c26e8e815198188edd13714e26714344
Chris D'Aloisio <chris@creativecontrol.com.au>
2018-05-18 00:28:53 +1000

    refactor specs

--- spec/interfacer/structural/adapter_spec.rb
+++ spec/interfacer/structural/adapter_spec.rb
@@ -1,29 +1,31 @@
-RSpec.describe Interfacer::Adapter do
-  describe '.build' do
-    subject(:build) do
-      described_class.build(:car, with_interface: :abstract_vehicle, for_class: :scooter) do
-        start do
-          "START"
-        end
+module Interfacer
+  RSpec.describe Adapter do
+    describe '.build' do
+      subject(:build) do
+        described_class.build(:car, with_interface: :abstract_vehicle, for_class: :scooter) do
+          start do
+            "START"
+          end
 
-        stop do
-          "STOP"
-        end
+          stop do
+            "STOP"
+          end
 
-        protected
+          protected
 
-        reset do
-          "RESET"
+          reset do
+            "RESET"
+          end
         end
       end
-    end
 
-    it 'works', :aggregate_failures do
-      Interfacer::Interface.build(:abstract_vehicle) do
-        def_public_methods(:start, :stop)
-        def_protected_methods(:reset)
+      it 'works', :aggregate_failures do
+        Interface.build(:abstract_vehicle) do
+          def_public_methods(:start, :stop)
+          def_protected_methods(:reset)
+        end
+        expect(build).to eq ScooterToCarAdapter
       end
-      expect(build).to eq ScooterToCarAdapter
     end
   end
 end
